538f2d8de06af671dfc8be266cef828f87b59b78
Sébastien <imbertsebastiendev@gmail.com>
2023-05-13 13:59:15 +0200

    ajout map et positionnement du joueur

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('hello');
+const canvas = document.querySelector('canvas');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+const c = canvas.getContext('2d');
+
+c.fillStyle = '#fff';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = './map.png';
+
+const playerImage = new Image();
+playerImage.src = './playerDown.png';
+
+image.onload = () => {
+  c.drawImage(image, -850, -300);
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height,
+  );
+}
